9ff9b10bb0a81e3a14562b190a11147e17bea841
Aka4806 <kaa4806@mail.ru>
2025-11-01 21:07:07 +0300

    Delete Лабы/lab_1 directory

--- Лабы/lab_1/lab_2/lab_python_oop/__init__.py
+++
@@ -1,7 +0,0 @@
-from .rectangle import Rectangle
-from .circle import Circle
-from .square import Square
-from .color import Color
-from .figure import Figure
-
-__all__ = ['Rectangle', 'Circle', 'Square', 'Color', 'Figure']
--- Лабы/lab_1/lab_2/lab_python_oop/square.py
+++
@@ -1,9 +0,0 @@
-from .rectangle import Rectangle
-
-class Square(Rectangle):
-    def __init__(self, side, color):
-        super().__init__(side, side, color)
-        self.side = side
-
-    def __repr__(self):
-        return f"Square(side={self.side}, color={self.color.color})"
